9f3e946c3c8ee67575b41cf656c70cffaeac5143
Elmer Galo <elmer.galo@outlook.com>
2024-02-26 06:36:35 -0500

    add custom modal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
+import { useState } from "react";
+
+import Modal from "./components/modal";
 import Button from "./components/button";
 
 import "./App.css";
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(true);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <main>
-      <Button label="Upload Document" />
+      <Button label="Upload Document" onClick={handleOpenModal} />
+      <Modal
+        isModalOpen={isModalOpen}
+        handleCloseModal={handleCloseModal}
+        modalWidth="lg"
+      ></Modal>
     </main>
   );
 }
---
+++ src/components/modal/index.tsx
@@ -0,0 +1,41 @@
+interface ModalProps {
+  handleCloseModal: () => void;
+  isModalOpen: boolean;
+  children?: React.ReactNode;
+  modalWidth: "md" | "lg";
+}
+
+const ModalWidth = {
+  lg: "w-4/5",
+  md: "w-1/2",
+};
+
+const Modal = ({
+  isModalOpen,
+  handleCloseModal,
+  children,
+  modalWidth = "md",
+}: ModalProps) => {
+  const classNames = ModalWidth[modalWidth];
+
+  if (!isModalOpen) return null;
+
+  return (
+    <div className="fixed -inset-x-8 inset-2 z-50 flex justify-center min-w-[375px] sm:inset-10">
+      <div className="fixed inset-0 bg-black opacity-30"></div>
+      <div
+        className={`relative z-50 bg-slate-50 p-8 rounded-xl max-w-screen-lg shadow-md overflow-hidden ${classNames} `}
+      >
+        <span
+          className="absolute top-0 left-0 px-2 m-5 rounded-md shadow-sm cursor-pointer bg-blue text-white text-xl"
+          onClick={handleCloseModal}
+        >
+          &times;
+        </span>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
